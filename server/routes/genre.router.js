de2c6281800ebbed522934c9a06dda24ea3e2628
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-14 10:33:56 -0500

    added fetchAllGenres GET request

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const query = `SELECT * FROM genres ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all genres', err);
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
