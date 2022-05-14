0d605da501aa06d9a4c8d7c6d1ded5bd673905fe
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-14 09:45:15 -0500

    started getDescription function in MovieItem

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,8 +1,13 @@
 function MovieItem(prop) {
+
+    const getDescription = () => {
+
+    }
+
   return (
     <div key={prop.movie.id}>
       <h3>{prop.movie.title}</h3>
-      <img src={prop.movie.poster} alt={prop.movie.title} />
+      <img src={prop.movie.poster} alt={prop.movie.title} onClick={getDescription} />
     </div>
   );
 }
